251e1ca5527a9ef603e0123a79af8949d9e33ab3
Rishat <81845@mail.ru>
2021-10-23 18:30:47 +0300

    first commit

---
+++ lib/urb.rb
@@ -0,0 +1,6 @@
+require "urb/version"
+
+module Urb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/urb_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Urb do
+  it "has a version number" do
+    expect(Urb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
